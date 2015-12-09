3f06da92b9fa138bbe9d51e0851b2f4b1111eb57
isaclee <isac.lee@utexas.edu>
2015-12-09 15:26:25 -0500

    updated cli.py, added run script

--- fft/cli.py
+++ fft/cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import fftmatch as fft
 import boyermoore as bm
 import argparse
@@ -5,10 +7,10 @@ import argparse
 parser = argparse.ArgumentParser(description='Search for a substring in a \
 genome')
 
-# Algorithm flag: Options= nlogn, nlogm, boyer moore; Default=nlogn
+# Algorithm flag: Options= nlogn, nlogm, boyer moore; Default=nlogm
 parser.add_argument('-a','--algorithm', choices=["nlogn", "nlogm", "boyermoore"],
-                    default='nlogn', nargs='?', help='The algorithm that you \
-want to run the search on. Default=nlogn')
+                    default='nlogm', nargs='?', help='The algorithm that you \
+want to run the search on. Default=nlogm')
 
 # Pattern arg: substring to search genomes for.
 parser.add_argument('pattern', help='The pattern that you want to search for in\
@@ -35,15 +37,15 @@ for genome_fn in args.genomes:
 if args.algorithm == 'nlogn':
     for gn in genomes:
         matches = fft.fft_match_index_n_log_n(genomes[gn], args.pattern[0])
-        print gn, ': Found matches at indices', matches
+        print gn, ': Found matches at indices', matches.tolist()
     pass
 elif args.algorithm == 'nlogm':
     for gn in genomes:
         matches = fft.fft_match_index_n_log_m(genomes[gn], args.pattern[0])
-        print gn, ': Found matches at indices', matches
+        print gn, ': Found matches at indices', matches.tolist()
     pass
 elif args.algorithm == 'boyermoore':
     for gn in genomes:
         matches = bm.boyer_moore_match_index(genomes[gn], args.pattern[0])
-        print gn, ': Found matches at indices', matches
+        print gn, ': Found matches at indices', matches.tolist()
     pass
---
+++ fft/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+date
+./cli.py -a nlogn CAG ../reference/huntington/huntington.fa > huntingtin_CAGs_nlogn.txt
+date
